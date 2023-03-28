6465ba4fa108c2988ee0e2e328e97492457f7532
Francis Bell <francisbell@Franciss-MBP.askey.com>
2023-03-28 10:46:39 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "Mini Messageboard", messages: messages });
+});
+
+/* GET form page. */
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: "Mini Messageboard"});
+});
+
+// Post data 
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.message, user: req.body.name, added: new Date()});
+  res.redirect('/');
+});
+
+module.exports = router;
